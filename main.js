1b5372d4b374f6db294cb1a2dd3fdbf675e1aa33
rmanikandan2001 <rmanikandan6867@gmail.com>
2023-08-15 23:15:36 -0700

    add

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+		height= prompt("enter a height");
+		
+		if(height<150){
+			console.log("Dwart person");
+		}
+		
+		else if(height>150 & height<=165){
+			console.log("average person");
+		}
+		
+		else if(height>165 & height<=195){
+			console.log("tallese person");
+		}
+		
+		else if(height>195){
+			console.log("abnormal person");
+		}
